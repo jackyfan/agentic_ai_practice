f85d57c78b1d211e6768f9fa00b7ef16c639482e
jacky <jackyfan@163.com>
2026-02-11 17:40:03 +0800

    大模型调用方法

---
+++ context_engineering_for_multi_agent_systems/commons/helpers.py
@@ -0,0 +1,28 @@
+from http.client import responses
+
+
+def create_mcp_message(sender, content, metadata=None):
+    """
+    Create a standardized message for the MCP.
+    """
+    return {
+        "protocol_version": "1.0",
+        "sender": sender,
+        "content": content,
+        "metadata": metadata or {}
+    }
+
+
+def call_llm(system_prompt, user_prompt,client):
+    """
+    Call the LLM with the given prompt.
+    """
+    try:
+        response =  client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[{"role": "system", "content":system_prompt},
+                      {"role": "user", "content": user_prompt}]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"An error occurred with the API call: {e}"
